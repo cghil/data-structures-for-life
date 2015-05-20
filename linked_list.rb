871d38ef47641fff7cb87b7b15ac3fad38c278ce
cghil <cghilarducci@gmail.com>
2015-05-20 13:25:28 -0700

    placed stack into a new file

--- linked_list.rb
+++ linked_list.rb
@@ -1,25 +1,6 @@
 require 'pry'
 
-class Stack
-	attr_accessor :container
-	def initialize
-		@container = []
-	end
-	def peek
-		@container[-1] #looks at the last one
-	end
-	def pop
-		@container.delete_at(-1) #gives you the last element of a node
-	end
-	def push(value)
-		@container.push(value) #pushes at the end of the array
-	end
-
-	def is_empty?
-		peek == nil
-	end
-
-end
+require_relative 'stack'
 
 module Mixin
 	def [](position)
